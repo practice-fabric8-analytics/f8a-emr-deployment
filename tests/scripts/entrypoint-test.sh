fee313b658e4cb0113608c22de93371b0841b8fd
sunilk747 <sunilk747@gmail.com>
2019-02-22 01:16:50 +0530

    update requirements file

--- tests/scripts/entrypoint-test.sh
+++ tests/scripts/entrypoint-test.sh
@@ -21,5 +21,5 @@ echo "*****************************************"
 echo "*** Unit tests ***"
 echo "*****************************************"
 
-pytest --cov=/src/ --cov-report term-missing --cov-fail-under=$COVERAGE_THRESHOLD -vv /tests/unit_tests/
+pytest --cov=/src/ --cov-report term-missing --cov-fail-under=$COVERAGE_THRESHOLD -vv /tests/
 codecov --token=36977e37-70fd-4b77-8e4c-57a6762f948f
