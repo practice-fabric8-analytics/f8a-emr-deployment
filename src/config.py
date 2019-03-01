0a24172b1b0f1131ff635803ad5ddddf1f3ff749
sunilk747 <ssamal@redhat.com>
2019-03-02 01:08:11 +0530

    added endpoint for verion details

--- src/config.py
+++ src/config.py
@@ -2,3 +2,5 @@
 import os
 
 DEPLOYMENT_PREFIX = os.environ.get('DEPLOYMENT_PREFIX', 'dev')
+AWS_S3_ACCESS_KEY_ID = os.environ.get('AWS_S3_ACCESS_KEY_ID', '')
+AWS_S3_SECRET_KEY_ID = os.environ.get('AWS_S3_SECRET_ACCESS_KEY', '')
