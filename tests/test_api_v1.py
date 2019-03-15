2b9894668772d49ea826d2a27e96c7e7ed0d2426
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-03-15 14:38:10 +0100

    Test for liveness endpoint

--- tests/test_api_v1.py
+++ tests/test_api_v1.py
@@ -15,3 +15,11 @@ def test_heart_beat_endpoint(client):
     json_data = get_json_from_response(response)
     assert "status" in json_data
     assert json_data["status"] == "ready"
+
+
+def test_liveness_endpoint(client):
+    """Test the liveness endpoint."""
+    response = client.get("/api/v1/liveness")
+    assert response.status_code == 200
+    json_data = get_json_from_response(response)
+    assert not json_data
