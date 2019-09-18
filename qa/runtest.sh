e90ac0860865fe60b2c755491d4f210b49e087a6
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-09-18 15:22:29 +0200

    Script to run unit tests

--- qa/runtest.sh
+++ qa/runtest.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+
+SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
+
+pushd "${SCRIPT_DIR}/.." > /dev/null
+
 TEST_IMAGE_NAME='emr-deployment-tests'
 
 check_python_version() {
@@ -24,3 +29,5 @@ else
     # CI instance will be torn down anyway, don't need to waste time on gc
     docker run ${TEST_IMAGE_NAME}
 fi
+
+popd > /dev/null
